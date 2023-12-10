666573614c7d2bea34be06cf26fae1aa1e54cf91
Vladimir Fibe <motiw@icloud.com>
2023-12-10 13:53:25 +0600

    locationButton

---
+++ README.md
@@ -0,0 +1,3 @@
+#  Wheather
+
+[Tutorial](https://www.softcover.io/read/7671f69a/ios-tutorial-book/6-weathery)
--- Weathery/ViewController.swift
+++ Weathery/ViewController.swift
@@ -1,19 +1,53 @@
-//
-//  ViewController.swift
-//  Weathery
-//
-//  Created by Vladimir Fibe on 09.12.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    let backgroundView: UIImageView = {
+        $0.image = .background
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.contentMode = .scaleAspectFill
+        return $0
+    }(UIImageView())
+
+    let locationButton: UIButton = {
+        $0.setImage(
+            UIImage(systemName: "location.circle.fill"),
+            for: []
+        )
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UIButton())
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupViews()
+        setupConstraints()
     }
-
-
 }
 
+extension ViewController {
+    private func setupViews() {
+        [backgroundView,
+         locationButton
+        ].forEach {
+            view.addSubview($0)
+        }
+    }
+
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+
+            locationButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            locationButton.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            locationButton.widthAnchor.constraint(equalToConstant: 44),
+            locationButton.heightAnchor.constraint(equalToConstant: 44)
+        ])
+    }
+}
+
+#Preview {
+    ViewController()
+}
