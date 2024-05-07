784722652e0e2ae605bebd0832ad522918365106
Vladimir Fibe <motiw@icloud.com>
2024-05-07 07:31:58 +0500

    Add SwiftUI View

--- Weathery/AppDelegate.swift
+++ Weathery/AppDelegate.swift
@@ -1,4 +1,4 @@
-import UIKit
+import SwiftUI
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -7,7 +7,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = WeatherViewController()
+//        window?.rootViewController = WeatherViewController()
+        window?.rootViewController = UIHostingController(rootView: WeatheryView())
         window?.makeKeyAndVisible()
         return true
     }
---
+++ Weathery/WeatheryView.swift
@@ -0,0 +1,45 @@
+import SwiftUI
+
+struct WeatheryView: View {
+    var body: some View {
+        Image("background")
+            .resizable()
+            .scaledToFill()
+            .ignoresSafeArea()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Preview {
+    WeatheryView()
+}
